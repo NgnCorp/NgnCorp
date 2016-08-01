6fb80df1ed5a5327960476f158f58a6fa382f0c8
ValeOfWar <lcf666666lcf@gmail.com>
2016-08-01 13:43:15 +0300

    1

--- src/ngn/controller/GasStation.java
+++ src/ngn/controller/GasStation.java
@@ -1,9 +1,166 @@
 package ngn.controller;
 
+import java.awt.event.ActionEvent;
+import java.util.Locale;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+
 /**
  *
  * @author Офис
  */
 public class GasStation {
+
+    private static SerialPort KolonkaCOM3;
+    static int komanda;
+    static javax.swing.Timer KolonkaStart;
+    static javax.swing.Timer ZaderzkaDoza;
+    static String PolozheniePistoleta;
+    static String SchetLitrov;
+    static String MoneySchetLitrov;
+    static String OtvetPoDoze;
+    static String OtvetKolonki;
+
+    public GasStation() {
+        KolonkaCOM3 = new SerialPort("COM3");
+        try {
+            KolonkaCOM3.openPort();
+            KolonkaCOM3.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_MARK);
+            KolonkaCOM3.setEventsMask(SerialPort.MASK_RXCHAR);
+            KolonkaCOM3.addEventListener(new EventListener());
+            TimerKolonkaStart();
+        } catch (SerialPortException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void TimerZaderzkaDoza(String komDoza) {
+        String doza = komDoza;
+        ZaderzkaDoza = new javax.swing.Timer(600, (ActionEvent e) -> {
+            try {
+                ZaderzkaDoza.stop();
+                komanda = 1;
+                KolonkaCOM3.writeString(doza); // Отправляем на колонку количество литров на отдачу
+                System.out.println("Отдали дозу");
+            } catch (SerialPortException ex) {
+                System.out.println(ex);
+            }
+        });
+        KolonkaStart.stop();
+        ZaderzkaDoza.restart();  // Запуск команды "ЗАПРОС СОСТОЯНИЯ"
+    }
+
+    public static void TimerKolonkaStart() {
+        KolonkaStart = new javax.swing.Timer(600, (ActionEvent e) -> {
+            try {
+                KolonkaCOM3.writeString("@10510045#");
+                komanda = 0;
+            } catch (SerialPortException ex) {
+                System.out.println(ex);
+            }
+        });
+        KolonkaStart.restart();  // Запуск команды "ЗАПРОС СОСТОЯНИЯ"
+    }
+
+    private class EventListener implements SerialPortEventListener {
+
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            if (event.isRXCHAR() && event.getEventValue() != 0) {
+                String data = "";
+                String oneSymbol;
+                try {
+                    String StartWork = KolonkaCOM3.readString(1);
+                    if (StartWork.indexOf("@") == 0) {
+                        // Get data //
+                        do {
+                            oneSymbol = KolonkaCOM3.readString(1);
+                            data += oneSymbol;
+                        } while (!"#".equals(oneSymbol));
+                        OtvetKolonki = StartWork + data;
+                        Integer dataLenghth = OtvetKolonki.length();
+                        String dataOffCrx = new String(OtvetKolonki.toCharArray(), 0, dataLenghth - 3);
+                        String controlNumber = new String(OtvetKolonki.toCharArray(), dataLenghth - 3, 2);
+
+                        // Check data //
+                        int decnum = dataOffCrx.length();
+                        char[] decimal = new char[decnum];
+                        int crc = 0;
+                        for (int col = 0; col < decnum; col++) {
+                            decimal[col] = dataOffCrx.charAt(col);
+                            crc ^= decimal[col]; // CRC (контрольная сумма)
+                        }
+                        String pihex = Integer.toHexString(crc);
+                        if (pihex.equals(controlNumber)) {
+                            System.out.println(OtvetKolonki);
+                        }
+                        if (komanda == 0) {
+                            switch (OtvetKolonki.indexOf("#")) {
+                                case 11:
+                                    if (OtvetKolonki.equals("@0151010044#")) {
+                                        PolozheniePistoleta = "ПИСТОЛЕТ ПОВЕШЕН";
+                                    } else {
+                                        PolozheniePistoleta = "ПОВЕСЬТЕ ПИСТОЛЕТ!";
+                                    }
+                                    break;
+                                //String poluchenieOtcheta = KolonkaCOM3.readString(8);
+                                //String schetLitrov = proverkaSvyazi + poluchenieOtcheta;
+                                ////////////////////////////Читаем 19 байт и проверяем наличие решетки в конце//////////////////////////////////////
+                                case 19:
+                                    //System.out.println(schetLitrov);
+                                    String hexNUM = new String(OtvetKolonki.toCharArray(), 9, 8);
+                                    double litrbez = Integer.decode("0x" + hexNUM) / 100.0;
+                                    PolozheniePistoleta = "ИДЕТ ПРОЦЕСС ЗАПРАВКИ...";
+                                    SchetLitrov = String.valueOf(litrbez);
+                                    //MoneySchetLitrov = String.format(Locale.ENGLISH, "%(.2f", NgnApp.customerPrice * litrbez);
+                                    break;
+                                default:
+                                    komanda = 1;
+                                    break;
+                            }
+                        }
+                        if (komanda == 1) {
+                            try {
+                                //OtvetPoDoze = KolonkaCOM3.readString(11);
+                                if (OtvetKolonki.equals("@0144010141#")) {
+                                    KolonkaCOM3.writeString("@1047010142#"); //PUSK
+                                    TimerKolonkaStart();
+                                    System.out.println("ДАЛИ ПУСК");
+                                } else {
+                                    //System.out.println(OtvetPoDoze);
+                                    //ZaderzkaDoza.restart();
+                                }
+                            } catch (SerialPortException ex) {
+                            }
+                        }
+                    }
+                } catch (SerialPortException ex) {
+                }
+            }
+        }
+    }
+
+    public static void StopStartCom3(boolean value) {
+        if (value) {
+            KolonkaStart.stop();
+        } else {
+            KolonkaStart.restart();
+        }
+    }
+
+    public static void CustomerInfoToZero() {
+        SchetLitrov = "";
+        MoneySchetLitrov = "";
+    }
+
+    public static void KomandaStop() {
+        try {
+            KolonkaCOM3.writeString("@015801014C#");
+            System.out.println("СТОП КОЛОНКА");
+        } catch (SerialPortException ex) {
+        }
+    }
     
 }
