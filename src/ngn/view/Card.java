a89b74c8313aa3adb784a270246a43808c876d5a
ValeOfWar <lcf666666lcf@gmail.com>
2016-08-01 14:24:34 +0300

    CardCss

--- src/ngn/view/Card.java
+++ src/ngn/view/Card.java
@@ -14,47 +14,16 @@ public class Card {
     private final javax.swing.JPasswordField CardCode;
     private final javax.swing.JLabel CardAnimate;
     private final javax.swing.JLabel CardText;
-    java.awt.GridBagConstraints gridBagConstraints;
+    
     public Card(JFrame JF) {
 
         EnterCard = new javax.swing.JPanel();
         CardCode = new javax.swing.JPasswordField();
         CardAnimate = new javax.swing.JLabel();
         CardText = new javax.swing.JLabel();
-
-        EnterCard.setBackground(new java.awt.Color(204, 0, 0));
-        EnterCard.setLayout(new java.awt.GridBagLayout());
-
-        CardCode.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        CardCode.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        CardCode.setToolTipText("");
-        CardCode.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                //CardCodeActionPerformed(evt);
-            }
-        });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.ipadx = 324;
-        gridBagConstraints.ipady = 24;
-        EnterCard.add(CardCode, gridBagConstraints);
-
+        
+        Css.cssCard(EnterCard, CardCode, CardAnimate, CardText);
         CardAnimate.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/firstAnim.gif"))); // NOI18N
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 1;
-        EnterCard.add(CardAnimate, gridBagConstraints);
-
-        CardText.setFont(new java.awt.Font("Candara", 1, 48)); // NOI18N
-        CardText.setForeground(new java.awt.Color(255, 255, 255));
-        CardText.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        CardText.setText("Поднесите карту к клавиатуре");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 2;
-        EnterCard.add(CardText, gridBagConstraints);
 
         JF.add(EnterCard);
     }
--- src/ngn/view/Css.java
+++ src/ngn/view/Css.java
@@ -4,7 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import ngn.Ngn;
 
 /**
@@ -13,6 +15,7 @@ import ngn.Ngn;
  */
 public class Css extends Ngn {
     
+    static java.awt.GridBagConstraints gridBagConstraints;
     static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     public static int windowHeight = screenSize.height;
     public static int windowWidth = screenSize.width;
@@ -30,9 +33,47 @@ public class Css extends Ngn {
         //window.setLocationRelativeTo(null);
         MFrame.setVisible(true);
     }
+    
     public static void JPanel(JPanel JP) {
         JP.setSize(windowWidth, windowHeight);
         JP.setBackground(new Color(204, 0, 0));
+    }
+
+    static void cssCard(JPanel EnterCard, JPasswordField CardCode, JLabel CardAnimate, JLabel CardText) {
+
+        EnterCard.setBackground(new java.awt.Color(204, 0, 0));
+        EnterCard.setLayout(new java.awt.GridBagLayout());
+
+        CardCode.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        CardCode.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        CardCode.setToolTipText("");
+        CardCode.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                //CardCodeActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 324;
+        gridBagConstraints.ipady = 24;
+        EnterCard.add(CardCode, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        EnterCard.add(CardAnimate, gridBagConstraints);
+
+        CardText.setFont(new java.awt.Font("Candara", 1, 48)); // NOI18N
+        CardText.setForeground(new java.awt.Color(255, 255, 255));
+        CardText.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        CardText.setText("Поднесите карту к клавиатуре");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        EnterCard.add(CardText, gridBagConstraints);
+        
     }
     
 }
