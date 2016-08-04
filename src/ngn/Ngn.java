74b3933c85fc0a87cec44700ddec1f2d5672a27c
ValeOfWar <lcf666666lcf@gmail.com>
2016-08-04 17:23:05 +0300

    senc

--- src/ngn/Ngn.java
+++ src/ngn/Ngn.java
@@ -18,7 +18,6 @@ public class Ngn extends JFrame {
 
         if (PreLoader.PreLoader()) {
             invokeLater(() -> {
-<<<<<<< HEAD
             // Controllers //
             
             KeyPad KEYPAD = new KeyPad();
@@ -40,27 +39,6 @@ public class Ngn extends JFrame {
             
             //ChangePanel.ChangePanel(Pin.EnterPin, Card.EnterCard);
         });
-=======
-                // Controllers //
-
-                KeyPad KEYPAD = new KeyPad();
-                GasStation GASSTATION = new GasStation();
-                Listener ACTIONLISTENER = new Listener();
-                Timer TIMER = new Timer();
-                Variables VARIABLES = new Variables();
-
-                // Frames //
-                JFrame NGN = new JFrame();
-                Css.MainFrame(NGN);
-
-                Work WORK = new Work(NGN);
-                Litrs LITRS = new Litrs(NGN);
-                Pin PIN = new Pin(NGN);
-                Card CARD = new Card(NGN);
-                
-                ChangePanel.ChangePanel(Pin.EnterPin, Card.EnterCard);
-            });
->>>>>>> origin/master
         }
     }
 }
