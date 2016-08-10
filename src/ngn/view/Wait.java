95b8ff1f8b533d7bcd835afc941a0464135742c4
Aleksandr <svitlyk.oleksandr@mail.ru>
2016-08-10 11:55:14 +0300

    1

--- src/ngn/view/Wait.java
+++ src/ngn/view/Wait.java
@@ -13,17 +13,14 @@ public class Wait {
         public static javax.swing.JPanel Waiting;
         public static javax.swing.JLabel Here;
         public static javax.swing.JLabel WaitingSeconds;
-        public static javax.swing.JLabel ClockImg;
         
     public Wait(JFrame JF) {
         
         Waiting = new javax.swing.JPanel();
         Here = new javax.swing.JLabel();
         WaitingSeconds = new javax.swing.JLabel();
-        ClockImg = new javax.swing.JLabel();
         
         Css.cssWait(Waiting, Here, WaitingSeconds);
-        ClockImg.setIcon(new ImageIcon(getClass().getResource("/images/clock.png"))); // NOI18N
         
         JF.add(Waiting);
         
