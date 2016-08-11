d0af7879ac02b5011fcade1c7d8ba6b3b74af6a4
Aleksandr <svitlyk.oleksandr@mail.ru>
2016-08-11 17:40:00 +0300

    1

--- src/ngn/controller/Listener.java
+++ src/ngn/controller/Listener.java
@@ -48,6 +48,7 @@ public class Listener {
                         String.valueOf(Variables.limitLitrs),
                         String.valueOf(Variables.usedLimitLitrs)
                     };
+                    writeWithoutInternet.writeWithoutInternet(Variables.ClientInfo);
                     /*
                     System.out.println(
                             "customerId: " + Variables.customerId
--- src/ngn/controller/writeWithoutInternet.java
+++ src/ngn/controller/writeWithoutInternet.java
@@ -14,8 +14,9 @@ public class writeWithoutInternet {
     public static void writeWithoutInternet(String[] Arr) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("/Data/FillingData.txt"))) {
             for (String item : Arr) {
-                bw.write(item+",");
+                bw.write(item + ",");
             }
+            bw.write("\r\n");
         } catch (IOException ex) {
             ex.getMessage();
         }
