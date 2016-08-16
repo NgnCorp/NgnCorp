95ba352a8bbaff5c94f3a5b49d2ac99121358b08
Aleksandr <svitlyk.oleksandr@mail.ru>
2016-08-16 13:55:53 +0300

    1

--- src/Preload/PreLoader.java
+++ src/Preload/PreLoader.java
@@ -8,6 +8,7 @@ public class PreLoader {
     // Обновление приложения
     // Запись в БД даных записаных без интернета на модуле
     // Ожидание ответа сервера
+    
     public static boolean PreLoader() {
         PortCheck.PortCheck();
         return InternetConn.InternetConn();
