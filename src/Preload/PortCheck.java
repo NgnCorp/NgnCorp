589a625da8d72ecdafc65340ee8b9fb06eaadcdd
ValeOfWar <lcf666666lcf@gmail.com>
2016-08-16 13:29:22 +0300

    зсpc

--- src/Preload/PortCheck.java
+++ src/Preload/PortCheck.java
@@ -1,5 +1,6 @@
 package Preload;
 
+import java.util.Arrays;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
@@ -9,43 +10,44 @@ import jssc.SerialPortList;
  * @author Офис
  */
 public class PortCheck {
-        static String[] portNames;
-        public static String GSPort;
-        public static String KPPort;
+
+    static String[] portNames;
+    static SerialPort PortToCheck;
+    public static String GSPort;
+    public static String KPPort;
 
     public static void PortCheck() {
         portNames = SerialPortList.getPortNames();
-            for (String portName : portNames) {
-                System.out.println(portName);
-                /*
-                try {
-                    serialPort.openPort();
-                    serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_MARK);
-                    serialPort.writeString("@10510045#");
-                    String oneChar = serialPort.readString(1);
-                    if (oneChar.equals("@")) {
-                        GSPort = portName;
+        for (String portName : portNames) {
+            PortToCheck = new SerialPort(portName);
+            try {
+                PortToCheck.openPort();
+                PortToCheck.getLinesStatus();
+                PortToCheck.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_MARK);
+                PortToCheck.writeString("@10510045#");
+                String oneChar = PortToCheck.readString(1);
+                if (oneChar.equals("@")) {
+                    GSPort = portName;
+                } else {
+                    PortToCheck.setParams(2400, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+                    oneChar = PortToCheck.readHexString(1);
+                    if (oneChar.contains("25")) {
+
                     } else {
-                        serialPort.setParams(2400, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-                        oneChar = serialPort.readHexString(1);
-                        if (oneChar.contains("25")) {
-                            
-                        } else {
-                            System.out.println("s");
-                        }
+                        System.out.println("s");
                     }
-                    serialPort.closePort();
-                }catch (SerialPortException ex) {
-                    //System.out.println(ex);
                 }
-*/
+                PortToCheck.closePort();
+            } catch (SerialPortException ex) {
+                //System.out.println(ex);
             }
-        if (GSPort == null){
+        }
+        if (GSPort == null) {
             System.out.println("Kolonka ne podkluchena");
         }
-        if(KPPort == null) {
+        if (KPPort == null) {
             System.out.println("Klava ne podkluchena");
         }
-            System.out.println(GSPort + " " + KPPort);
+        System.out.println(GSPort + " " + KPPort);
     }
 }
