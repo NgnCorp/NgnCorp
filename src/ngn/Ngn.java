4c21acb7e1abf027b88b52621de1544ef973b6a5
ValeOfWar <lcf666666lcf@gmail.com>
2016-08-16 20:09:49 +0300

    tt

--- src/ngn/Ngn.java
+++ src/ngn/Ngn.java
@@ -1,10 +1,12 @@
 package ngn;
 
-import Preload.PreLoader;
 import static java.awt.EventQueue.invokeLater;
+import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 import ngn.view.*;
 import ngn.controller.*;
+import static ngn.view.BeforeStart.LoadingText;
 
 /**
  *
@@ -12,32 +14,40 @@ import ngn.controller.*;
  */
 public class Ngn extends JFrame {
 
+    static Timer AppStart;
+
     public static void main(String[] args) {
 
         // Frames //
         JFrame NGN = new JFrame();
         Css.MainFrame(NGN);
         BeforeStart BEFORESTART = new BeforeStart(NGN);
-        
-        if (PreLoader.PreLoader()) {
-            invokeLater(() -> {
 
-                Card CARD = new Card(NGN);
-                Pin PIN = new Pin(NGN);
-                Litrs LITRS = new Litrs(NGN);
-                Work WORK = new Work(NGN);
-                Wait WAIT = new Wait(NGN);
-                Info INFO = new Info(NGN);
-                Load LOAD = new Load(NGN);
-                Bye BYE = new Bye(NGN);
+        AppStart = new Timer(1000, (ActionEvent e) -> {
+            
+            if (LoadingText.getText().equals("Настройка завершена")) {
+                invokeLater(() -> {
 
-                // Controllers //
-                KeyPad KEYPAD = new KeyPad();
-                GasStation GASSTATION = new GasStation();
-                Listener ACTIONLISTENER = new Listener();
-                Timers TIMER = new Timers();
-                Variables VARIABLES = new Variables();
-            });
-        }
+                    Card CARD = new Card(NGN);
+                    Pin PIN = new Pin(NGN);
+                    Litrs LITRS = new Litrs(NGN);
+                    Work WORK = new Work(NGN);
+                    Wait WAIT = new Wait(NGN);
+                    Info INFO = new Info(NGN);
+                    Load LOAD = new Load(NGN);
+                    Bye BYE = new Bye(NGN);
+
+                    // Controllers //
+                    KeyPad KEYPAD = new KeyPad();
+                    GasStation GASSTATION = new GasStation();
+                    Listener ACTIONLISTENER = new Listener();
+                    Timers TIMER = new Timers();
+                    Variables VARIABLES = new Variables();
+                });
+            } else {
+                AppStart.restart();
+            }
+        });
+        AppStart.start();
     }
 }
