176771d0eed90b44651222123a9deae981d65353
ValeOfWar <lcf666666lcf@gmail.com>
2016-10-06 12:44:34 +0300

    InternetConnect bugfix

--- src/Preload/InternetConn.java
+++ src/Preload/InternetConn.java
@@ -1,6 +1,7 @@
 package Preload;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
@@ -11,13 +12,13 @@ import java.net.*;
  */
 public class InternetConn {
 
+    /*
     public static boolean InternetConn() {
         Boolean result = false;
         try {
             URL url = new URL("http://google.com/");
             InputStream inputStream = url.openStream();
-            BufferedReader reader;
-            reader = new BufferedReader(new InputStreamReader(inputStream, "windows-1251"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "windows-1251"));
             StringBuilder allText = new StringBuilder();
             char[] buff = new char[1];
 
@@ -25,15 +26,34 @@ public class InternetConn {
             while ((count = reader.read(buff)) != -1) {
                 allText.append(buff, 0, count);
             }
-            Integer indStart = allText.indexOf("<title>");
-            Integer indEnd = allText.indexOf("</title>", indStart);
-            String google = allText.substring(indStart + 7, indEnd);
+            Integer intStart = allText.indexOf("<title>");
+            Integer intEnd = allText.indexOf("</title>", intStart);
+            String google = allText.substring(intStart + 7, intEnd);
             if ("Google".equals(google)) {
                 result = true;
             }
+        reader.close();
         } catch (Exception e) {
             System.out.println(e);
         }
         return result ;
+    }
+     */
+    public static boolean InternetConn() {
+        try {
+            URL url = new URL("http://www.google.com/");
+            HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+            urlc.setRequestProperty("User-Agent", "test");
+            urlc.setRequestProperty("Connection", "close");
+            urlc.setConnectTimeout(1000);
+            urlc.setReadTimeout(2000);
+            urlc.connect();
+            System.out.println(urlc.getResponseCode());
+            return urlc.getResponseCode() == 200;
+
+        } catch (IOException e) {
+            System.out.println("Ошибка проверки подключения к интернету: " + e);
+            return false;
+        }
     }
 }
