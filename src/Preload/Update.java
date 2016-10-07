de912cbf2ebfd49e395e81d92c89f23838d81bf7
ValeOfWar <lcf666666lcf@gmail.com>
2016-10-07 08:56:14 +0300

    ver.0.15

--- src/Preload/Update.java
+++ src/Preload/Update.java
@@ -20,7 +20,7 @@ import static ngn.view.BeforeStart.BSLoadingText;
 
 public class Update {
 
-    private static final Double VER = 0.14;
+    private static final Double VER = 0.15;
 
     private static final String URL = Config.URL;
     private static final String USER = Config.USER;
